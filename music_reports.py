11a5959650a0c5ecb0d006c1cb37132cf5e387c8
Michael Lechowicz <ichal.lechowicz@gmail.com>
2019-07-23 12:04:40 +0200

    add function by_oldest_yongest and display albums

--- music_reports.py
+++ music_reports.py
@@ -53,7 +53,8 @@ def count_albums_genre(list_albums, genre): #count albums by genre
         if disc[3] == genre:
             amount += 1
     return amount
-        
+
+
 def list_to_string(list_item, length): #prints strings instead of list
     element_albums = 0
     str_item = ""
@@ -61,24 +62,29 @@ def list_to_string(list_item, length): #prints strings instead of list
     while element_albums < 5:
 
         format_str += ("{:>" + str(length[element_albums]) + "} ")
-        element_albums+=1
-    str_item = format_str.format(list_item[0],list_item[1], list_item[2], list_item[3],list_item[4])
+        element_albums += 1
+    str_item = format_str.format(list_item[0], list_item[1], list_item[2], list_item[3],list_item[4])
     
     return str_item
 
 # find_albums_genre(list_albums, "progressive rock")
 
+
 def find_time_range(list_albums, list_range):    #format_str.format(str_it    #format_str.format(str_item = str_item + list_item[element_albums] + " ")t_item[3],list_item[4])em = str_item + list_item[element_albums] + " ")t_item[3],list_item[4])
     length = length_of_albums(list_albums, "empty")
     print(length)
     for disc in list_albums:
-        if int(disc[2]) in range(list_range[0],list_range[1]):
-            print(list_to_string(disc,length))
-            
-def sorting_albums(list_albums):
-    pass
+        if int(disc[2]) in range(list_range[0], list_range[1]):
+            print(list_to_string(disc, length))
+
+
 def take_time(element):
     return element[4]
+
+
+def take_date(element):
+    return int(element[2])
+
 
 def shortest_longest(list_albums, is_long):
     index_album = 0
@@ -87,7 +93,7 @@ def shortest_longest(list_albums, is_long):
         (m, s) = time.split(':')
         list_albums[index_album][4] = int(m) *60 + int(s)
         index_album += 1
-    list_albums.sort(key = take_time)
+    list_albums.sort(key=take_time)
     index_album = 0
     for album in list_albums:
         list_albums[index_album][4] = str(datetime.timedelta(seconds = list_albums[index_album][4]))
@@ -98,9 +104,10 @@ def shortest_longest(list_albums, is_long):
         print("The shortest album is: " + str(list_albums[0][1]) + " " +str(list_albums[0][4]))
 
 
-# shortest_longest(list_albums, False)
+# shortest_longest(list_albums, True)
 
-def by_artist(list_albums, name_artist):
+
+def by_artist(list_albums, name_artist): # show album by particular artist
     length = length_of_albums(list_albums, name_artist)
     print(length)
     for disc in list_albums:
@@ -108,16 +115,28 @@ def by_artist(list_albums, name_artist):
             print(list_to_string(disc, length))
 
 
-def by_album_name(list_albums, name_album):
+def by_album_name(list_albums, name_album): # show album by album name
     length = length_of_albums(list_albums, name_album)
     print(length)
     for disc in list_albums:
         if disc[1] == name_album:
             print(list_to_string(disc, length))
-def oldest_youngest(list_albums):
-    pass
-def play_albums(list_albums):
-    pass
+
+
+def oldest_youngest(list_albums, is_young):# show album oldest/youngest
+    list_albums.sort(key=take_date)
+    print(list_albums)
+    if is_young:
+        print("The youngest album is: " + str(list_albums[-1][1]) + " " + str(list_albums[-1][2]))
+    else:
+        print("The oldest album is: " + str(list_albums[0][1]) + " " + str(list_albums[0][2]))
+
+
+def display_albums(list_albums):# display all albums
+    length = length_of_albums(list_albums, "empty")
+    print(length)
+    for disc in list_albums:
+        print(list_to_string(disc, length))
 
 # additional functions
 
@@ -136,10 +155,14 @@ def save_to_file():
 
 list_range = [0, 1980]
 
-#find_time_range(list_albums, list_range)
+# find_time_range(list_albums, list_range)
 
-print(list_albums)
+# print(list_albums)
 
 # by_artist(list_albums, "Pink Floyd")
 
-by_album_name(list_albums, "The Dark Side Of The Moon")
+# by_album_name(list_albums, "The Dark Side Of The Moon")
+
+# oldest_youngest(list_albums, True)
+
+display_albums(list_albums)
